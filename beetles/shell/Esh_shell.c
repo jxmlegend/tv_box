5ff4aeb5302bbe13178567db8824a18b16e00f7b
gzq <guozhiqiang@yekertech.com>
2017-04-24 18:37:21 +0800

    删除开机logo

--- beetles/shell/Esh_shell.c
+++ beetles/shell/Esh_shell.c
@@ -59,21 +59,6 @@ static __s32 __show_logo(void)
 	__disp_layer_info_t layer_para;
     __u32 arg[3];
     __s32 ret;
-    
-	//parse logo bmp	
-	picture = (Picture_t *)eLIBs_malloc(sizeof(Picture_t));
-	if(picture == NULL)
-	{
-		__wrn("malloc logo bmp buffer failed!\n");
-		return EPDK_FAIL;
-	}
-    eLIBs_memset(picture, 0, sizeof(Picture_t));
-    
-	ret = Parse_Pic_BMP_ByPath(BEETLES_APP_ROOT"res\\boot_ui\\logo.bmp", picture, NULL);
-	if(-1 == ret)
-	{
-        return -1;
-	}
 	
 	/*open display driver handle*/
 	p_disp = eLIBs_fopen("b:\\DISP\\DISPLAY", "r+");
@@ -82,63 +67,6 @@ static __s32 __show_logo(void)
 		Esh_Wrn("open display device fail!\n");
 		return EPDK_FAIL;
 	}	
-
-	width  = eLIBs_fioctrl(p_disp, DISP_CMD_SCN_GET_WIDTH, 0, 0);
-	height = eLIBs_fioctrl(p_disp, DISP_CMD_SCN_GET_HEIGHT, 0, 0);
-
-	__msg("width = %d, height = %d\n", width, height);
-#if LOGO_USE_SCLAER_MODE
-	arg[0] = DISP_LAYER_WORK_MODE_SCALER;
-#else
-    arg[0] = DISP_LAYER_WORK_MODE_NORMAL;
-#endif
-	arg[1] = 0;
-    arg[2] = 0;
-    logo_layer = eLIBs_fioctrl(p_disp, DISP_CMD_LAYER_REQUEST, 0, (void*)arg);
-
-#if LOGO_USE_SCLAER_MODE
-	layer_para.mode = DISP_LAYER_WORK_MODE_SCALER;
-#else
-    layer_para.mode = DISP_LAYER_WORK_MODE_NORMAL;
-#endif
-    layer_para.fb.addr[0]       = (__u32)picture->Buffer; 
-    layer_para.fb.size.width    =  picture->Width;
-    layer_para.fb.size.height    = picture->Height;
-    layer_para.fb.mode          = DISP_MOD_INTERLEAVED;
-    layer_para.fb.format        = DISP_FORMAT_ARGB8888;
-    layer_para.fb.br_swap       = 0;
-    layer_para.fb.seq           = DISP_SEQ_ARGB;
-    layer_para.ck_enable        = 0;
-    layer_para.alpha_en         = 1; 
-    layer_para.alpha_val        = 0xff;
-    layer_para.pipe             = 0; 
-    layer_para.src_win.x        = 0;
-    layer_para.src_win.y        = 0;
-    layer_para.src_win.width    = picture->Width;
-    layer_para.src_win.height   = picture->Height;
-#if LOGO_USE_SCLAER_MODE
-    layer_para.scn_win.x        = 0;
-    layer_para.scn_win.y        = 0;
-    layer_para.scn_win.width    = width;
-    layer_para.scn_win.height   = height;
-#else
-    layer_para.scn_win.x        = (width-picture->Width)>>1;
-    layer_para.scn_win.y        = (height-picture->Height)>>1;
-    layer_para.scn_win.width    = picture->Width;
-    layer_para.scn_win.height   = picture->Height;
-#endif
-
-	__msg("scn_win.x = %d, scn_win.y = %d\n", layer_para.scn_win.x, layer_para.scn_win.y);
-    __msg("picture->Width = %d, picture->Height = %d\n", picture->Width, picture->Height);
-    arg[0] = logo_layer;
-    arg[1] = (__u32)&layer_para;
-    arg[2] = 0;
-    eLIBs_fioctrl(p_disp,DISP_CMD_LAYER_SET_PARA,0,(void*)arg);
-
-    arg[0] = logo_layer;
-    arg[1] = 0;
-    arg[2] = 0;
-    eLIBs_fioctrl(p_disp,DISP_CMD_LAYER_OPEN,0,(void*)arg);
 
 	eLIBs_fioctrl(p_disp, DISP_CMD_LCD_ON, 0, 0);
 	
